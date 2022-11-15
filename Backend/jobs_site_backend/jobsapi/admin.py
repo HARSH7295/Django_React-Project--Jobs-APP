f3d0ff3b440029ac9e2bc91cf6bcdbd34c5f02a3
HARSH7295 <75429960+HARSH7295@users.noreply.github.com>
2022-11-15 14:07:56 +0530

    first commit

---
+++ Backend/jobs_site_backend/jobsapi/admin.py
@@ -0,0 +1,7 @@
+from msilib.schema import CustomAction
+from django.contrib import admin
+from .models import CustomUser, Job
+# Register your models here.
+
+admin.site.register(Job)
+admin.site.register(CustomUser)
